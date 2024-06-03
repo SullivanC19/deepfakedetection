2c150d67a243075871b2de97fe4d35bc2823d3b3
aharshal623 <harshal.agrawal.623@gmail.com>
2024-06-02 23:04:52 -0700

    implemented normalization
    you can test it locally using test_normalization.py

--- main.py
+++ main.py
@@ -9,6 +9,26 @@ from models.cnn import cnn_model
 from train.trainer import train_cnn, test_cnn, train_svm, test_svm
 from sklearn.linear_model import SGDClassifier
 
+def compute_mean_and_std(dataset):
+    loader = DataLoader(dataset, batch_size=100, shuffle=False, num_workers=4)
+    mean = 0.0
+    std = 0.0
+    nb_samples = 0.0
+
+    for data in loader:
+        data = data[0]
+        batch_samples = data.size(0)
+        data = data.view(batch_samples, data.size(1), -1)
+        mean += data.mean(2).sum(0)
+        std += data.std(2).sum(0)
+        nb_samples += batch_samples
+
+    mean /= nb_samples
+    std /= nb_samples
+
+    return mean, std
+
+
 def main():
   # Define the path to your data csv 
   data_folder = './140k-real-and-fake-faces'
@@ -16,11 +36,15 @@ def main():
   val_dataset = 'dev_valid.csv'
   test_dataset = 'dev_test.csv'
 
+  dataset = CustomDataset(data_folder, train_dataset) 
+  mean, std = compute_mean_and_std(dataset)
+
   # Define the transformations to apply to the images
   transform = transforms.Compose([
       transforms.Resize((256, 256)),  # Resize the image to 256x256
       transforms.ToTensor(),  # Convert PIL image to tensor
-      transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize image pixels
+      transforms.Normalize(mean=mean.tolist(), std=std.tolist())  # Normalize image pixels
+     # transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize image pixels
   ])
 
   # Create an instance of your custom dataset
@@ -34,32 +58,37 @@ def main():
 
   # Iterate over the data loader to access batches of preprocessed images
   # for batch_idx, batch in enumerate(data_loader):
-    # Process each batch as needed
-    # batch is a tensor of shape (batch_size, channels, height, width)
-    # For example, you can perform some operations on the batch like printing its shape
-    # print(f"Batch {batch_idx + 1} shape:", batch[0].shape, len(batch[1]))
+  #   Process each batch as needed
+  #   batch is a tensor of shape (batch_size, channels, height, width)
+  #   For example, you can perform some operations on the batch like printing its shape
+  #   print(f"Batch {batch_idx + 1} shape:", batch[0].shape, len(batch[1]))
 
-    # # Convert the batch tensor to a NumPy array
-    # batch_np = batch[0].numpy()
+  #   # Convert the batch tensor to a NumPy array
+  #   batch_np = batch[0].numpy()
 
-    # # Iterate over images in the batch
-    # for i in range(len(batch_np)):
-    #   image_np = np.transpose(batch_np[i], (1, 2, 0))  # Change from (C, H, W) to (H, W, C)
-    #   # Display the image using matplotlib
-    #   plt.imshow(image_np)
-    #   plt.title(f"Batch {batch_idx + 1}, Image {i + 1}")
-    #   plt.axis('off')  # Turn off axis labels
-    #   plt.show()
-    #   print('label: ', batch[1][i])
+  #   # Iterate over images in the batch
+  #   for i in range(len(batch_np)):
+  #     image_np = np.transpose(batch_np[i], (1, 2, 0))  # Change from (C, H, W) to (H, W, C)
+  #     # Display the image using matplotlib
+  #     plt.imshow(image_np)
+  #     plt.title(f"Batch {batch_idx + 1}, Image {i + 1}")
+  #     plt.axis('off')  # Turn off axis labels
+  #     plt.show()
+  #     print('label: ', batch[1][i])
     
-    # Optionally, break the loop after processing a few batches
-    # if batch_idx == 4:
-      # using batch 5 as dev set (0 indexing)
-      # dev_x = batch[0].to(torch.float32).requires_grad_(True)
-      # dev_y = batch[1].to(torch.float32).requires_grad_(True)
-      # break
+  #   Optionally, break the loop after processing a few batches
+  #   if batch_idx == 4:
+  #     using batch 5 as dev set (0 indexing)
+  #     dev_x = batch[0].to(torch.float32).requires_grad_(True)
+  #     dev_y = batch[1].to(torch.float32).requires_grad_(True)
+  #     break
 
-  # dev_data = TensorDataset(dev_x, dev_y)
+  # dev_data = TensorDataset(dev_x, dev_y)'
+
+
+
+
+  
 
   print('Starting baseline')
   svm =  SGDClassifier(max_iter=1000, tol=1e-3)
