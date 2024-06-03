30ea2d4413b7eb873c5599b9f4fc088b19f406ae
SullivanC19 <colinsullivan203@gmail.com>
2024-06-03 13:57:23 -0700

    added data augmentation

---
+++ data/dataset.py
@@ -0,0 +1,42 @@
+import os
+import csv
+import torch
+import torchvision
+from PIL import Image
+
+from load_data import load_data_info
+
+class FaceImageDataset(torch.utils.data.Dataset):
+
+    preprocess = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((128, 128)),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(mean=0, std=1)
+    ])
+
+    augment = torchvision.transforms.Compose([
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.RandomRotation(180),
+        torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+        torchvision.transforms.RandomResizedCrop(128, scale=(0.8, 1.0)),
+    ])
+
+    def __init__(self, data_type: str, do_augment: bool=False):
+        self.data_type = data_type
+        self.do_augment = do_augment
+
+        image_info = load_data_info(data_type)
+        self.image_files = image_info['image'].values
+        self.image_labels = image_info['label'].values
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        image = Image.open(self.image_files[idx])
+
+        if self.do_augment:
+            image = self.augment(image)
+        image = self.preprocess(image)
+            
+        return (image, self.image_labels[idx])
--- data/load_data.py
+++ data/load_data.py
@@ -7,6 +7,7 @@ from .constants import URL_DATA, get_data_info_file, DIR_DATA
 def load_data_info(data_type):
   data_info = pd.read_csv(get_data_info_file(data_type))
   data_info['image'] = data_info['path'].apply(lambda path: os.path.join(DIR_DATA, path))
+  data_info['label'] = data_info['label'].astype(int)
   return data_info[['image', 'label']]
 
 def download_data():
