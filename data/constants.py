03891b1a9e648413d4483cf8351a26e551808bae
SullivanC19 <colinsullivan203@gmail.com>
2024-06-03 12:44:43 -0700

    refactor data loading

--- data/constants.py
+++ data/constants.py
@@ -1,3 +1,16 @@
-DIR_DATA_TRAIN = './140k-real-and-fake-faces/real_vs_fake/train'
-FAKE_DIR = 'fake'
-REAL_DIR = 'real'
+import os
+from typing import List
+
+DIR_DATA_INFO = './140k-real-and-fake-faces/'
+DIR_DATA = './140k-real-and-fake-faces/real_vs_fake/real-vs-fake/'
+URL_DATA = 'https://www.kaggle.com/datasets/xhlulu/140k-real-and-fake-faces'
+
+LABEL_FAKE = 0
+LABEL_REAL = 1
+
+DATA_TYPES = ['train', 'test', 'val']
+
+def get_data_info_file(data_type: str, data_info_dir: List[str]=DIR_DATA_INFO) -> str:
+    assert data_type in DATA_TYPES
+    return os.path.join(data_info_dir, f'{data_type}.csv')
+
