16b18881103c26a5da677ffd8af705ea12fdc718
SullivanC19 <colinsullivan203@gmail.com>
2024-06-04 13:06:29 -0700

    finished main python script

--- data/dataset.py
+++ data/dataset.py
@@ -13,14 +13,15 @@ from .constants import IMAGE_SIZE
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def compute_mean_and_std(dataset):
-    loader = dt.DataLoader(dataset, batch_size=64, shuffle=False, num_workers=8)
+    loader = dt.DataLoader(dataset, batch_size=64, shuffle=False)
     mean = torch.zeros(3)
     std = torch.zeros(3)
     num_samples = 0.0
 
     for (image, _) in tqdm(loader):
         batch_samples = image.size(0)
-        image = image.view(batch_samples, image.size(1), -1)
+        color_channels = image.size(1)
+        image = image.view(batch_samples, color_channels, -1)
         mean += image.mean(2).sum(0)
         std += image.std(2).sum(0)
         num_samples += batch_samples
@@ -52,29 +53,28 @@ class FaceImageDataset(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         image = Image.open(self.image_files[idx])
 
-        # TODO deal with complex numbers
-        # TODO deal with colors
-
-        preprocess = torchvision.transforms.Compose([
-            torchvision.transforms.Resize((128, 128)),
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize(mean=self.mean, std=self.std)
-        ])
-
         augment = torchvision.transforms.Compose([
             torchvision.transforms.RandomHorizontalFlip(),
             torchvision.transforms.RandomRotation(180),
             torchvision.transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
             torchvision.transforms.RandomResizedCrop(IMAGE_SIZE, scale=(0.8, 1.0)),
         ])
+        fft = lambda x: torch.fft.fft2(x).abs()
+        tensorize = torchvision.transforms.Compose([
+            torchvision.transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+            torchvision.transforms.ToTensor(),
+        ])
+        normalize = torchvision.transforms.Normalize(mean=self.mean, std=self.std)
 
         if self.do_augment:
             image = augment(image)
-        
-        image = preprocess(image)
 
+        image = tensorize(image)
+                
         if self.do_fft:
-            image = torch.log(torch.fft.fft2(image))
+            image = fft(image)
+
+        image = normalize(image)
 
         image = image.to(device)
         label = torch.tensor(self.image_labels[idx], dtype=torch.float).expand(1).to(device)
