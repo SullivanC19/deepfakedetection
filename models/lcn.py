5dcbe32e5d723b2f4d786cd31b0a97c84e6f40cd
Colin Patrick Sullivan <colins26@stanford.edu>
2024-06-04 20:55:26 -0700

    fixed env2

--- models/lcn.py
+++ models/lcn.py
@@ -1,8 +1,8 @@
-from torchvision.models import resnet50, ResNet50_Weights
+from torchvision.models import resnet50
 import torch.nn as nn
 
 def lcn_model():
-    model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
+    model = resnet50(pretrained=True)
     for param in model.parameters():
         param.requires_grad = False
         
